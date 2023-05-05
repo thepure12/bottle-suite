06a70f12c33e1356f9ab7c55c94d9cf7307d7068
thepure12 <renninger12@gmail.com>
2023-05-05 09:05:46 -0400

    added more tests

---
+++ tests/test_resource_factory.py
@@ -0,0 +1,13 @@
+from src.bottle_suite import resource_factory
+import unittest
+
+class TestResourceFactory(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_creatSQLResource(self):
+        pass
+
+    def test_createSQLiteResource(self):
+        pass
