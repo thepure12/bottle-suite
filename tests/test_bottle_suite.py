981b8a7a68b480476b6fa15047958507edfd0cde
thepure12 <renninger12@gmail.com>
2023-05-05 16:13:18 -0400

    added more tests

--- tests/test_bottle_suite.py
+++ tests/test_bottle_suite.py
@@ -2,6 +2,10 @@ import unittest
 import webtest
 from src.bottle_suite import BottleSuite
 import bottle_rest
+import bottle_jwt
+import toml
+import os
+os.chdir("tests")
 
 DEBUG = False
 
@@ -28,15 +32,20 @@ class TestResource(bottle_rest.Resource):
 class TestBottleSuite(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.bottle_suite = BottleSuite()
+        self.bottle_suite = BottleSuite(gen_res=False)
         self.app = webtest.TestApp(self.bottle_suite)
         
 
     def addTestResource(self):
-        self.bottle_suite.rest.addResource(TestResource, "/resource")
+        self.bottle_suite.rest.addResource(TestResource, "/test_resource")
 
     def test_init(self):
         pass
+
+    # Config
+    def test_loadConfig(self):
+        cfg = toml.load("bottle_suite.toml")
+        self.assertEqual(cfg, self.bottle_suite.cfg)
 
     # REST
     def test_initRest(self):
@@ -51,19 +60,32 @@ class TestBottleSuite(unittest.TestCase):
     def test_addResource(self):
         self.addTestResource()
         self.assertIn("TestResource", self.bottle_suite.resource_names)
+        self.assertEqual(200, self.app.get('/test_resource').status_code)
 
     def test_restMethods(self):
         self.addTestResource()
-        self.assertEqual(self.app.options("/resource").status_code, 200)
-        self.assertEqual(self.app.get("/resource").status_code, 200)
-        self.assertEqual(self.app.post("/resource").status_code, 200)
-        self.assertEqual(self.app.put("/resource").status_code, 200)
-        self.assertEqual(self.app.patch("/resource").status_code, 200)
-        self.assertEqual(self.app.delete("/resource").status_code, 200)
+        self.assertEqual(self.app.options("/test_resource").status_code, 200)
+        self.assertEqual(self.app.get("/test_resource").status_code, 200)
+        self.assertEqual(self.app.post("/test_resource").status_code, 200)
+        self.assertEqual(self.app.put("/test_resource").status_code, 200)
+        self.assertEqual(self.app.patch("/test_resource").status_code, 200)
+        self.assertEqual(self.app.delete("/test_resource").status_code, 200)
 
-    
+    def test_genFileResource(self):
+        self.bottle_suite = BottleSuite()
+        self.app = webtest.TestApp(self.bottle_suite)
+        self.assertIn("FileResource", self.bottle_suite.resource_names)
+        self.assertEqual(200, self.app.get("/file_resource").status_code)
 
     # JWT
+    def test_initJWT(self):
+        self.assertIsInstance(self.bottle_suite.jwt, bottle_jwt.JWTPlugin)
+
+
+    def test_initNoJWT(self):
+        self.bottle_suite = BottleSuite(jwt=False, cfg_file=None)
+        self.assertIsNone(next((p for p in self.bottle_suite.plugins if "JWT" in str(p)), None))
+
 
     # SQL
 
